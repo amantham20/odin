1ae1e03f015abd92e8aa4b8fcc00eef9aee935e9
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2022-07-29 00:49:01 -0400

    things are working now

--- client/src/components/problem/problem.jsx
+++ client/src/components/problem/problem.jsx
@@ -7,6 +7,8 @@ import Editor from "@monaco-editor/react";
 import ReactResizeDetector from 'react-resize-detector';
 import MonacoEditor from 'react-monaco-editor';
 
+import * as monaco from 'monaco-editor';
+
 import './markdown.css'
 import './problem.css'
 
@@ -26,15 +28,13 @@ const Problem = () => {
     })
   }, []);
 
-  const editorRef = React.useRef(null);
-
-  function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor;
-  }
-
-  function showValue() {
-    alert(editorRef.current?.getValue());
-  }
+  const editorOptions =  {
+      minimap: {
+        enabled: false
+      },
+      selectOnLineNumbers: true,
+      wordWrap: 'on'
+    };
 
   return (
     <div className='problemPage'>
@@ -42,18 +42,65 @@ const Problem = () => {
         <h1>Problems</h1>
         <ReactMarkdown className='markdown-body' children={MarkdownData}/>
       </div>
+      <div className='codearea'>
       <div className='editor'>
 
-      <Editor
-        defaultLanguage="javascript"
-        defaultValue="// some comment"
-        onMount={handleEditorDidMount}
-      />
+
+        <MOEditor editorOptions={editorOptions}/>
 
 
+      </div>
       </div>
     </div>
   )
 }
+
+class MOEditor extends React.Component {
+  constructor(props) {
+      super(props)
+
+      this.state = {
+          width: 0,
+          height: 0,
+      }
+      this.editor_div = React.createRef()
+
+      this.handle_rezise = this.handle_rezise.bind(this);
+  }
+
+  componentDidMount() {
+      const editor_model = monaco.editor.createModel('#Hey There this is aman', 'python');
+      this.monaco_editor = monaco.editor.create(this.editor_div.current, this.props.editorOptions);
+      this.monaco_editor.setModel(editor_model);
+  }
+
+  componentWillUnmount() {
+      this.monaco_editor && this.monaco_editor.dispose();
+  }
+
+  handle_rezise(width, height) {
+      this.monaco_editor.layout({ height, width });
+  }
+
+  render() {
+      return(
+          <div 
+              className="editor-container"
+              style={{ height: '100%' }}>
+              <ReactResizeDetector
+                  handleWidth
+                  handleHeight
+                  onResize={ this.handle_rezise }
+                  refreshMode="debounce"
+                  refreshRate={100} />
+              <div 
+                  className="editor"
+                  ref={ this.editor_div }
+                  style={{ height: '100%' }} />
+          </div>
+      )
+  }
+}
+
 
 export default Problem
