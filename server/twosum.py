9f7a3165812cd702cbe745dc0cfac9346c446cef
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2022-07-31 23:04:22 -0400

    changes

--- server/twosum.py
+++ server/twosum.py
@@ -1,11 +1,9 @@
 #Hey There this is aman 
 def twosum(arr, target):
-	# print(arr,target)
-	d = {}
-	for i, v in enumerate(arr):
-		if v in d:
-			print(d[v], i)
-			return d[v], i
-		
-		d[target - v] = i
+	temp = {}
 
+	for i, v in enumerate(arr):
+		if v in temp:
+			return temp[v], i
+		temp[target - v] = i
+		print(v, i)
